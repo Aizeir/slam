3bfebdcc43d5b38b2dec9304008644e28f9855b5
Aizeir <95878962+Aizeir@users.noreply.github.com>
2025-03-09 11:32:35 +0100

    au cas ou

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import math
+import time
 import pygame as pg
 from pygame.math import Vector2 as vec2
 pg.init()
 pg.display.set_caption("SLAM")
+
 
 
 W,H = 600,600
@@ -31,14 +34,14 @@ class Sim:
         self.robot.draw()
 
 
-def colliderect(pr, r, angle):
-    for a,b in [r.bottomright-vec2(r.bottomleft),r.topright-vec2(r.topleft), r.bottomleft-vec2(r.topleft),r.bottomright-vec2(r.topright)]:
-        for pv in [pr.bottomright-vec2(pr.bottomleft),pr.topright-vec2(pr.topleft), pr.bottomleft-vec2(pr.topleft),pr.bottomright-vec2(pr.topright)]:
-            pv = pv.rotate(angle)
-
 robot_size = vec2(20,20)*cm
 robot_speed = 30 # en cm/s
 robot_turn_speed = 50 # en deg/s
+robot_wheel_rad = 4*cm
+robot_diag = robot_size.magnitude() / 2
+
+bit_num = 2*16
+angle_quad = 360 / bit_num * 1.5
 
 class Robot:
     def __init__(self, sim, pos):
@@ -49,8 +52,12 @@ class Robot:
         self.moving = 0
         self.angle = 0
         self.speed = vec2()
+        self.wheel_angles = [0]*4
 
-
+        # odométrie
+        self.wheel_bits = [(0,0)]*4
+        self.wheel_bits_old = [(0,0)]*4
+        self.wheel_bits_change = [0]*4
 
         # dessin
         self.chassis = pg.Surface(robot_size).convert_alpha() # pr les rotation
@@ -59,24 +66,45 @@ class Robot:
 
     def update(self):
         k = pg.key.get_pressed()
+
+        # Movement
         self.moving = k[pg.K_UP] - k[pg.K_DOWN]
-        self.angle += (k[pg.K_RIGHT] - k[pg.K_LEFT]) * self.sim.dt * robot_turn_speed * (not self.moving)
-        self.speed = robot_speed*cm*vec2(0,-1).rotate(self.angle)*self.moving
-        self.mvt_collision()
+        self.speed = self.moving*robot_speed*cm*vec2(0,-1).rotate(self.angle) * self.sim.dt
 
-    def mvt_collision(self):
-        self.rect.x += self.speed.x * self.sim.dt
+        # Collision
+        rect = self.rect.move(self.speed)
         for r in self.sim.rects:
-            if r.colliderect(self.rect):
-                if self.speed.x > 0: self.rect.right = r.x
-                else: self.rect.x = r.right
+            if r.colliderect(rect):
+                self.speed = vec2()
+                self.moving = False
+                break
+        else:
+            self.rect = rect
 
-        self.rect.y += self.speed.y * self.sim.dt
-        for r in self.sim.rects:
-            if r.colliderect(self.rect):
-                if self.speed.y > 0: self.rect.bottom = r.y
-                else: self.rect.y = r.bottom
+        # Turn
+        angle = (not self.moving)*(k[pg.K_RIGHT] - k[pg.K_LEFT]) * robot_turn_speed * self.sim.dt
+        self.angle += angle
 
+        # Wheel angles
+        for i in range(4):
+            distance_px = self.moving*self.speed.magnitude() + (-1,1,1,-1)[i]*rad(angle)*robot_diag
+            self.wheel_angles[i] += deg(distance_px / robot_wheel_rad)
+
+        # Measure
+        self.odometry()
+
+    def odometry(self):
+        # Relever les bits mesurés
+        self.wheel_bits_old = self.wheel_bits.copy()
+        for i in range(4):
+            bit = (self.wheel_angles[i]%360 // bit_num)%2
+            bit2 = ((self.wheel_angles[i]+angle_quad)%360 // bit_num)%2
+            self.wheel_bits[i] = (bit,bit2)
+
+            # Bit changed
+            if self.wheel_bits[i] != self.wheel_bits_old[i]:
+                self.wheel_bits_change[i] = time.time_ns()
+                self.wheel_states[i] = 
 
     def draw(self):
         # chassis (on anticipe les rotations)
@@ -84,8 +112,18 @@ class Robot:
         self.sim.display.blit(chassis, chassis.get_rect(center=self.rect.center))
 
         # chassis prédit
+        debug([[int(a) for a in x] for x in self.wheel_bits])
 
 
+debug_font = pg.font.Font(None,35)
+def debug(info, y=16, x=16, surf=None, font=debug_font):
+    surf = pg.display.get_surface()
+    debug_surf = font.render(str(info),True,"white")
+    debug_rect = debug_surf.get_rect(topleft = (x,y))
+    pg.draw.rect(surf, "black", debug_rect.inflate(12,0))
+    surf.blit(debug_surf, debug_rect.topleft)
+def deg(r): return r * 180 / math.pi
+def rad(d): return d * math.pi / 180
 
 s = Sim()
 while True:
